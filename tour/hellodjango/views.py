5355a78d2d9f2c0c744ca0ffcbc3f05b6892a4da
kgastronaunt <kgastronaunt@gmail.com>
2019-02-25 11:35:21 +0600

    Template

--- tour/hellodjango/views.py
+++ tour/hellodjango/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Django!!!")
+    now = datetime.now()
+    return render(
+        request,
+        "hellodjango/index.html",
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
